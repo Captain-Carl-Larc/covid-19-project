aebf62a9ca2e3631072b2cfc2bb2cf02d4d7e922
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-13 22:53:21 +0300

    added death rate column

--- index.py
+++ index.py
@@ -11,3 +11,6 @@ critical_columns = ['total_cases', 'total_deaths', 'total_vaccinations']
 df_filtered = df_filtered.dropna(subset=['date'] + critical_columns)
 numeric_cols = df_filtered.select_dtypes(include=['float64', 'int64']).columns
 df_filtered[numeric_cols] = df_filtered[numeric_cols].interpolate()
+
+# Add death rate column
+df_filtered['death_rate'] = df_filtered['total_deaths'] / df_filtered['total_cases']
